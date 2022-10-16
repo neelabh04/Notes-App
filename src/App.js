94aa6edde6463b7ded8d515b043d5be83210d878
Neelabh Singh <neelabh041997@gmail.com>
2022-10-16 09:43:30 +0530

    Delete note added

--- src/App.js
+++ src/App.js
@@ -32,14 +32,21 @@ const App = () => {
       id: nanoid(),
       text: text,
       date: date.toLocaleDateString(),
-    }
+    };
 
+    
     const newNotes = [...notes, newNote];
-    setNotes(newNotes); 
-  }
+    setNotes(newNotes);
+  };
+
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note)=> note.id !== id);
+    setNotes(newNotes);
+  };
+  
   return (
     <div className="container">
-      <NotesList notes={notes} handleAddNote={addNote}/>
+      <NotesList notes={notes} handleAddNote={addNote} handleDeleteNote={deleteNote} />
     </div>
   );
 };
