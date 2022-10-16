5e8a928aa9b2f67dacb88385fcd6bd46d631635d
Neelabh Singh <neelabh041997@gmail.com>
2022-10-16 10:15:04 +0530

    Search note achieved

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { nanoid } from "nanoid";
 import NotesList from "./components/NotesList";
+import Search from "./components/Search";
 
 const App = () => {
   const [notes, setNotes] = useState([
@@ -26,6 +27,8 @@ const App = () => {
     },
   ]);
 
+  const [searchText, setSearchText] = useState("");
+
   const addNote = (text) => {
     const date = new Date();
     const newNote = {
@@ -34,19 +37,25 @@ const App = () => {
       date: date.toLocaleDateString(),
     };
 
-    
     const newNotes = [...notes, newNote];
     setNotes(newNotes);
   };
 
   const deleteNote = (id) => {
-    const newNotes = notes.filter((note)=> note.id !== id);
+    const newNotes = notes.filter((note) => note.id !== id);
     setNotes(newNotes);
   };
-  
+
   return (
     <div className="container">
-      <NotesList notes={notes} handleAddNote={addNote} handleDeleteNote={deleteNote} />
+      <Search handleSearchNote={setSearchText} />
+      <NotesList
+        notes={notes.filter((note) =>
+          note.text.toLowerCase().includes(searchText)
+        )}
+        handleAddNote={addNote}
+        handleDeleteNote={deleteNote}
+      />
     </div>
   );
 };
